0afff558dbf7c8a63e71517ff9df245892ad0590
cartnovaco-netizen <cartnova.co@gmail.com>
2026-03-31 21:41:45 +0530

    Simplified OAuth auth path to use default GitHub app redirect setting

--- api/auth.js
+++ api/auth.js
@@ -3,7 +3,7 @@ module.exports = function handler(req, res) {
   if (!GITHUB_CLIENT_ID) {
     return res.status(500).send("OAUTH_CLIENT_ID environment variable is missing.");
   }
-  const redirect_uri = `https://${req.headers.host}/api/callback`;
-  const url = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=repo,user&redirect_uri=${redirect_uri}`;
+  // GitHub handles redirect_uri from the OAuth App settings
+  const url = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=repo,user`;
   res.redirect(302, url);
 }
